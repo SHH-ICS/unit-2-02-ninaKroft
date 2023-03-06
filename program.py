f55d1a94262f51eb7079a93aa3339dc12daf7ac0
Nina Kroft <124377480+ninaKroft@users.noreply.github.com>
2023-03-06 15:34:18 +0000

    variables maybe

--- program.py
+++ program.py
@@ -1,5 +1,5 @@
 # My Python Program
 # Task:  Use the function myFunction to output a simple "Hello World!" statement
 
-#def myFunction():
-    print("Hello World!")
+myText = "Hello, World!"
+    print(myText)
