ece0c4196eb53b632eadb001ae27793f05639555
Nina Kroft <124377480+ninaKroft@users.noreply.github.com>
2023-03-07 15:06:31 +0000

    done extra

--- program.py
+++ program.py
@@ -1,8 +1,17 @@
 # My Python Program
 # Task:  Use the function myFunction to output a simple "Hello World!" statement
 
+# extra solution
 def myFunction():
     name = input()
     return name
 
 print("Hello,", myFunction())
+
+
+#initial solution
+#def myFunction():
+#    myText = "Hello World!"
+#    return myText
+
+#print(myFunction())
